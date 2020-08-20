428128ee246ed934738bedee802a4341e4ad14e5
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-08-20 18:02:59 +0530

    Update Cucumber to version 6.5.0.

--- src/test/java/com/nelkinda/template/acceptance/AcceptanceTest.java
+++ src/test/java/com/nelkinda/template/acceptance/AcceptanceTest.java
@@ -1,14 +1,13 @@
 package com.nelkinda.template.acceptance; // TODO change
 
-import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        strict = true,
         tags = "not(@wip) and not(@future)"
 )
-@RunWith(Cucumber.class)
+@RunWith(io.cucumber.junit.Cucumber.class)
+@io.cucumber.junit.platform.engine.Cucumber
 public class AcceptanceTest {
 }
